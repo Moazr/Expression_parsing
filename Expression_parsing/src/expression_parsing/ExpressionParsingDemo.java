84d5ee8b1763c0d63d4005c5cee925cc6c2ab4a4
Mohammed Alzarea <Mmo7ammadd@gmail.com>
2024-05-16 23:33:58 +0300

    EP-17 test

---
+++ Expression_parsing/src/expression_parsing/ExpressionParsingDemo.java
@@ -0,0 +1,19 @@
+package expression_parsing;
+// test 
+
+import javax.swing.JFrame;
+
+
+public class ExpressionParsingDemo {
+
+    
+        public static void main(String[] args) {
+        GUI l = new GUI();
+        l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        l.setSize(700,330);
+        l.setVisible(true);
+        l.setResizable(false);
+    }
+    
+    
+}
